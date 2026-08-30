034957badbfad78f85fb1d71256be99c77afad0d
Mubles Pinky <nacholetto21@gmail.com>
2026-08-30 18:02:51 -0400

    Create productos.js

---
+++ productos.js
@@ -0,0 +1,113 @@
+const productos = [
+
+    {
+        id: 1,
+        nombre: "Mueble de madera de haya con ratán natural",
+        codigo: "DA402",
+        medidas: "30 × 30 × 60 cm",
+        precio: 32000,
+        categoria: "Muebles",
+        imagen: "DA402.jpg"
+    },
+
+    {
+        id: 2,
+        nombre: "Mueble de madera de haya con ratán natural",
+        codigo: "DA403",
+        medidas: "30 × 30 × 79 cm",
+        precio: 40000,
+        categoria: "Muebles",
+        imagen: "DA403.jpg"
+    },
+
+    {
+        id: 3,
+        nombre: "Mueble de madera de haya con ratán natural",
+        codigo: "DA404",
+        medidas: "60 × 35 × 75 cm",
+        precio: 54000,
+        categoria: "Muebles",
+        imagen: "DA404.jpg"
+    },
+
+    {
+        id: 4,
+        nombre: "Mueble de madera de haya con ratán natural",
+        codigo: "DA405",
+        medidas: "60 × 35 × 90 cm",
+        precio: 65000,
+        categoria: "Muebles",
+        imagen: "DA405.jpg"
+    },
+
+    {
+        id: 5,
+        nombre: "Mueble de madera de haya con ratán natural",
+        codigo: "DA406",
+        medidas: "74 × 35 × 81 cm",
+        precio: 83000,
+        categoria: "Muebles",
+        imagen: "DA406.jpg"
+    },
+
+    {
+        id: 6,
+        nombre: "Mueble de madera de haya con ratán natural",
+        codigo: "DA408",
+        medidas: "80 × 35 × 86 cm",
+        precio: 86000,
+        categoria: "Muebles",
+        imagen: "DA408.jpg"
+    },
+
+    {
+        id: 7,
+        nombre: "Mueble de madera de haya con ratán natural",
+        codigo: "DA409",
+        medidas: "30 × 27 × 43 cm",
+        precio: 32000,
+        categoria: "Muebles",
+        imagen: "DA409.jpg"
+    },
+
+    {
+        id: 8,
+        nombre: "Mueble de madera de haya con ratán natural",
+        codigo: "DA410",
+        medidas: "30 × 27 × 61 cm",
+        precio: 32000,
+        categoria: "Muebles",
+        imagen: "DA410.jpg"
+    },
+
+    {
+        id: 9,
+        nombre: "Mueble de madera de haya con ratán natural",
+        codigo: "DA411",
+        medidas: "30 × 27 × 78 cm",
+        precio: 40000,
+        categoria: "Muebles",
+        imagen: "DA411.jpg"
+    },
+
+    {
+        id: 10,
+        nombre: "Mueble de madera de haya con ratán natural",
+        codigo: "DA412",
+        medidas: "100 × 38 × 70 cm",
+        precio: 135000,
+        categoria: "Muebles",
+        imagen: "DA412.jpg"
+    },
+
+    {
+        id: 11,
+        nombre: "Mueble de madera de haya con ratán natural",
+        codigo: "DA413",
+        medidas: "100 × 35 × 90 cm",
+        precio: 135000,
+        categoria: "Muebles",
+        imagen: "DA413.jpg"
+    }
+
+];
