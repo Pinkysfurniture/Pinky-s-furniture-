f78f86ec7b523d03074999bb7f2bf20f5f46a4b7
Mubles Pinky <nacholetto21@gmail.com>
2026-08-31 12:42:35 -0400

    Update Productos.js

--- Sillas/Productos.js
+++ Sillas/Productos.js
@@ -2,122 +2,492 @@ const productosSillas = [
 
     {
         id: 1,
-        nombre: "Silla",
-        codigo: "CODIGO-01",
-        medidas: "",
-        precio: 0,
+        nombre: "Silla Danubio PP",
+        codigo: "S001",
+        medidas: "43 × 43 × 92 cm",
+        precio: 31000,
         categoria: "Sillas",
         imagen: "imagenes/sillas/silla-01.jpg"
     },
 
     {
         id: 2,
-        nombre: "Silla",
-        codigo: "CODIGO-02",
-        medidas: "",
-        precio: 0,
+        nombre: "Silla Delta Negra",
+        codigo: "S002",
+        medidas: "45 × 43,5 × 80 cm",
+        precio: 37000,
         categoria: "Sillas",
         imagen: "imagenes/sillas/silla-02.jpg"
     },
 
     {
         id: 3,
-        nombre: "Silla",
-        codigo: "CODIGO-03",
-        medidas: "",
-        precio: 0,
+        nombre: "Silla Delta Blanca",
+        codigo: "S003",
+        medidas: "45 × 43,5 × 80 cm",
+        precio: 37000,
         categoria: "Sillas",
         imagen: "imagenes/sillas/silla-03.jpg"
     },
 
     {
         id: 4,
-        nombre: "Silla",
-        codigo: "CODIGO-04",
-        medidas: "",
-        precio: 0,
+        nombre: "Silla Dallas",
+        codigo: "S004",
+        medidas: "41 × 42 × 84 cm",
+        precio: 36000,
         categoria: "Sillas",
         imagen: "imagenes/sillas/silla-04.jpg"
     },
 
     {
         id: 5,
-        nombre: "Silla",
-        codigo: "CODIGO-05",
-        medidas: "",
-        precio: 0,
+        nombre: "Silla Chiavari Blanca",
+        codigo: "S005",
+        medidas: "46 × 39 × 92 cm",
+        precio: 31000,
         categoria: "Sillas",
         imagen: "imagenes/sillas/silla-05.jpg"
     },
 
     {
         id: 6,
-        nombre: "Silla",
-        codigo: "CODIGO-06",
-        medidas: "",
-        precio: 0,
+        nombre: "Silla Chiavari Negra",
+        codigo: "S006",
+        medidas: "46 × 39 × 92 cm",
+        precio: 31000,
         categoria: "Sillas",
         imagen: "imagenes/sillas/silla-06.jpg"
     },
 
     {
         id: 7,
-        nombre: "Silla",
-        codigo: "CODIGO-07",
-        medidas: "",
-        precio: 0,
+        nombre: "Silla Love Blanca",
+        codigo: "S007",
+        medidas: "40 × 39 × 92 cm",
+        precio: 32000,
         categoria: "Sillas",
         imagen: "imagenes/sillas/silla-07.jpg"
     },
 
     {
         id: 8,
-        nombre: "Silla",
-        codigo: "CODIGO-08",
-        medidas: "",
-        precio: 0,
+        nombre: "Silla Love Negra",
+        codigo: "S008",
+        medidas: "40 × 39 × 92 cm",
+        precio: 32000,
         categoria: "Sillas",
         imagen: "imagenes/sillas/silla-08.jpg"
     },
 
     {
         id: 9,
-        nombre: "Silla",
-        codigo: "CODIGO-09",
-        medidas: "",
-        precio: 0,
+        nombre: "Silla Normando Blanca",
+        codigo: "S009",
+        medidas: "40 × 39 × 92 cm",
+        precio: 32000,
         categoria: "Sillas",
         imagen: "imagenes/sillas/silla-09.jpg"
     },
 
     {
         id: 10,
-        nombre: "Silla",
-        codigo: "CODIGO-10",
-        medidas: "",
-        precio: 0,
+        nombre: "Silla France",
+        codigo: "S010",
+        medidas: "45 × 42 × 78,5 cm",
+        precio: 34000,
         categoria: "Sillas",
         imagen: "imagenes/sillas/silla-10.jpg"
     },
 
     {
         id: 11,
-        nombre: "Silla",
-        codigo: "CODIGO-11",
-        medidas: "",
-        precio: 0,
+        nombre: "Sitial France",
+        codigo: "S011",
+        medidas: "52,5 × 42 × 78,5 cm",
+        precio: 38000,
         categoria: "Sillas",
         imagen: "imagenes/sillas/silla-11.jpg"
     },
 
     {
         id: 12,
-        nombre: "Silla",
-        codigo: "CODIGO-12",
-        medidas: "",
-        precio: 0,
+        nombre: "Silla Chiavari Metal Silver",
+        codigo: "S012",
+        medidas: "39 × 41 × 88,5 cm",
+        precio: 36000,
         categoria: "Sillas",
         imagen: "imagenes/sillas/silla-12.jpg"
+    },
+
+    {
+        id: 13,
+        nombre: "Silla Chiavari Metal Dorada",
+        codigo: "S013",
+        medidas: "39 × 41 × 88,5 cm",
+        precio: 36000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-13.jpg"
+    },
+
+    {
+        id: 14,
+        nombre: "Silla Leon Beige",
+        codigo: "S014",
+        medidas: "52 × 53 × 81 cm",
+        precio: 32000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-14.jpg"
+    },
+
+    {
+        id: 15,
+        nombre: "Silla Leon Gris",
+        codigo: "S015",
+        medidas: "52 × 53 × 81 cm",
+        precio: 32000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-15.jpg"
+    },
+
+    {
+        id: 16,
+        nombre: "Silla Leon Negra",
+        codigo: "S016",
+        medidas: "52 × 53 × 81 cm",
+        precio: 32000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-16.jpg"
+    },
+
+    {
+        id: 17,
+        nombre: "Silla Murcia",
+        codigo: "S017",
+        medidas: "42,5 × 43 × 79 cm",
+        precio: 35000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-17.jpg"
+    },
+
+    {
+        id: 18,
+        nombre: "Silla Rex Roja",
+        codigo: "S018",
+        medidas: "50 × 43,5 × 82 cm",
+        precio: 40000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-18.jpg"
+    },
+
+    {
+        id: 19,
+        nombre: "Sitial Rex",
+        codigo: "S019",
+        medidas: "48 × 43,5 × 81 cm",
+        precio: 49000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-19.jpg"
+    },
+
+    {
+        id: 20,
+        nombre: "Silla King",
+        codigo: "S020",
+        medidas: "40 × 39 × 94 cm",
+        precio: 86000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-20.jpg"
+    },
+
+    {
+        id: 21,
+        nombre: "Silla Oficina Ergonómica UIC-602",
+        codigo: "UIC-602",
+        medidas: "46 × 37 cm / altura 92 cm",
+        precio: 45000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-21.jpg"
+    },
+
+    {
+        id: 22,
+        nombre: "Silla Oficina Queen",
+        codigo: "S022",
+        medidas: "42 × 42 cm / altura 83 cm",
+        precio: 90000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-22.jpg"
+    },
+
+    {
+        id: 23,
+        nombre: "Silla Oficina Queen Rosado",
+        codigo: "S023",
+        medidas: "42 × 42 cm / altura 83 cm",
+        precio: 90000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-23.jpg"
+    },
+
+    {
+        id: 24,
+        nombre: "Silla Capitoné Top Oficina",
+        codigo: "S024",
+        medidas: "43 cm profundidad / altura 80-90 cm",
+        precio: 65000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-24.jpg"
+    },
+
+    {
+        id: 25,
+        nombre: "Silla Capitoné Top Gold Oficina",
+        codigo: "S025",
+        medidas: "43 cm profundidad / altura 80-90 cm",
+        precio: 65000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-25.jpg"
+    },
+
+    {
+        id: 26,
+        nombre: "Silla Crossback Madera RW1801",
+        codigo: "RW1801",
+        medidas: "50 × 53 × 89 cm",
+        precio: 103000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-26.jpg"
+    },
+
+    {
+        id: 27,
+        nombre: "Taburete Bar Crossback Antique",
+        codigo: "S027",
+        medidas: "43 × 43 × 105 cm",
+        precio: 135000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-27.jpg"
+    },
+
+    {
+        id: 28,
+        nombre: "Taburete Bar Crossback Negro",
+        codigo: "S028",
+        medidas: "43 × 43 × 105 cm",
+        precio: 135000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-28.jpg"
+    },
+
+    {
+        id: 29,
+        nombre: "Taburete Bar Crossback Blanco",
+        codigo: "S029",
+        medidas: "43 × 43 × 105 cm",
+        precio: 135000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-29.jpg"
+    },
+
+    {
+        id: 30,
+        nombre: "Taburete Bar Andes",
+        codigo: "S030",
+        medidas: "42 × 42 × 105 cm",
+        precio: 135000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-30.jpg"
+    },
+
+    {
+        id: 31,
+        nombre: "Silla Luxury Gold Patas Doradas Beige",
+        codigo: "S031",
+        medidas: "50 × 45 × 82 cm",
+        precio: 67000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-31.jpg"
+    },
+
+    {
+        id: 32,
+        nombre: "Silla Luxury Gold Patas Doradas Café",
+        codigo: "S032",
+        medidas: "50 × 45 × 82 cm",
+        precio: 67000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-32.jpg"
+    },
+
+    {
+        id: 33,
+        nombre: "Silla Luxury Gold Patas Doradas Gris",
+        codigo: "S033",
+        medidas: "50 × 45 × 82 cm",
+        precio: 67000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-33.jpg"
+    },
+
+    {
+        id: 34,
+        nombre: "Silla Luxury Felpa Pata Negra Gris",
+        codigo: "S034",
+        medidas: "50 × 45 × 82 cm",
+        precio: 67000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-34.jpg"
+    },
+
+    {
+        id: 35,
+        nombre: "Silla Luxury Felpa Pata Negra Beige",
+        codigo: "S035",
+        medidas: "50 × 45 × 82 cm",
+        precio: 67000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-35.jpg"
+    },
+
+    {
+        id: 36,
+        nombre: "Silla Luxury Felpa Pata Negra Negra",
+        codigo: "S036",
+        medidas: "50 × 45 × 82 cm",
+        precio: 67000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-36.jpg"
+    },
+
+    {
+        id: 37,
+        nombre: "Sitial Boston Felpa",
+        codigo: "S037",
+        medidas: "40 × 40 × 80 cm",
+        precio: 63000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-37.jpg"
+    },
+
+    {
+        id: 38,
+        nombre: "Sitial Boston Antique",
+        codigo: "S038",
+        medidas: "40 × 42 × 80 cm",
+        precio: 65000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-38.jpg"
+    },
+
+    {
+        id: 39,
+        nombre: "Poltrona Queen Gris",
+        codigo: "S039",
+        medidas: "52 × 45 × 71 cm",
+        precio: 125000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-39.jpg"
+    },
+
+    {
+        id: 40,
+        nombre: "Poltrona Queen Beige",
+        codigo: "S040",
+        medidas: "52 × 45 × 71 cm",
+        precio: 125000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-40.jpg"
+    },
+
+    {
+        id: 41,
+        nombre: "Taburete Bar Queen Fijo Pata Gold Crudo",
+        codigo: "S041",
+        medidas: "38,5 × 42 × 88 cm",
+        precio: 108000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-41.jpg"
+    },
+
+    {
+        id: 42,
+        nombre: "Taburete Bar Queen Fijo Pata Gold Gris",
+        codigo: "S042",
+        medidas: "38,5 × 42 × 88 cm",
+        precio: 108000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-42.jpg"
+    },
+
+    {
+        id: 43,
+        nombre: "Taburete Bar Queen Fijo Pata Gold Rosado",
+        codigo: "S043",
+        medidas: "38,5 × 42 × 88 cm",
+        precio: 108000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-43.jpg"
+    },
+
+    {
+        id: 44,
+        nombre: "Taburete Bar Queen Fijo Pata Gold Negro",
+        codigo: "S044",
+        medidas: "38,5 × 42 × 88 cm",
+        precio: 108000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-44.jpg"
+    },
+
+    {
+        id: 45,
+        nombre: "Taburete Bar Queen Pata Negra Gris",
+        codigo: "S045",
+        medidas: "38,5 × 42 × 88 cm",
+        precio: 108000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-45.jpg"
+    },
+
+    {
+        id: 46,
+        nombre: "Taburete Bar Queen Pata Negra Rosa Viejo",
+        codigo: "S046",
+        medidas: "38,5 × 42 × 88 cm",
+        precio: 108000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-46.jpg"
+    },
+
+    {
+        id: 47,
+        nombre: "Taburete Bar Queen Pata Negra Negro",
+        codigo: "S047",
+        medidas: "38,5 × 42 × 88 cm",
+        precio: 108000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-47.jpg"
+    },
+
+    {
+        id: 48,
+        nombre: "Taburete Bar Queen Pata Negra Beige",
+        codigo: "S048",
+        medidas: "38,5 × 42 × 88 cm",
+        precio: 108000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-48.jpg"
+    },
+
+    {
+        id: 49,
+        nombre: "Taburete Bar Queen Pata Negra Rosado",
+        codigo: "S049",
+        medidas: "38,5 × 42 × 88 cm",
+        precio: 108000,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-49.jpg"
     }
 
 ];
