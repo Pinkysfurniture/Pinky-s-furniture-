7abcbf298d69d7417d4b0ac0da5ef82078752fcd
Mubles Pinky <nacholetto21@gmail.com>
2026-08-31 12:41:40 -0400

    Update Productos.js

--- Sillas/Productos.js
+++ Sillas/Productos.js
@@ -1,1 +1,123 @@
+const productosSillas = [
 
+    {
+        id: 1,
+        nombre: "Silla",
+        codigo: "CODIGO-01",
+        medidas: "",
+        precio: 0,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-01.jpg"
+    },
+
+    {
+        id: 2,
+        nombre: "Silla",
+        codigo: "CODIGO-02",
+        medidas: "",
+        precio: 0,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-02.jpg"
+    },
+
+    {
+        id: 3,
+        nombre: "Silla",
+        codigo: "CODIGO-03",
+        medidas: "",
+        precio: 0,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-03.jpg"
+    },
+
+    {
+        id: 4,
+        nombre: "Silla",
+        codigo: "CODIGO-04",
+        medidas: "",
+        precio: 0,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-04.jpg"
+    },
+
+    {
+        id: 5,
+        nombre: "Silla",
+        codigo: "CODIGO-05",
+        medidas: "",
+        precio: 0,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-05.jpg"
+    },
+
+    {
+        id: 6,
+        nombre: "Silla",
+        codigo: "CODIGO-06",
+        medidas: "",
+        precio: 0,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-06.jpg"
+    },
+
+    {
+        id: 7,
+        nombre: "Silla",
+        codigo: "CODIGO-07",
+        medidas: "",
+        precio: 0,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-07.jpg"
+    },
+
+    {
+        id: 8,
+        nombre: "Silla",
+        codigo: "CODIGO-08",
+        medidas: "",
+        precio: 0,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-08.jpg"
+    },
+
+    {
+        id: 9,
+        nombre: "Silla",
+        codigo: "CODIGO-09",
+        medidas: "",
+        precio: 0,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-09.jpg"
+    },
+
+    {
+        id: 10,
+        nombre: "Silla",
+        codigo: "CODIGO-10",
+        medidas: "",
+        precio: 0,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-10.jpg"
+    },
+
+    {
+        id: 11,
+        nombre: "Silla",
+        codigo: "CODIGO-11",
+        medidas: "",
+        precio: 0,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-11.jpg"
+    },
+
+    {
+        id: 12,
+        nombre: "Silla",
+        codigo: "CODIGO-12",
+        medidas: "",
+        precio: 0,
+        categoria: "Sillas",
+        imagen: "imagenes/sillas/silla-12.jpg"
+    }
+
+];
